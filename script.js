a7c32787d68796a19ec17fa806b0a558bad8b057
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-02-18 04:07:14 +0600

    Practice

--- script.js
+++ script.js
@@ -493,24 +493,18 @@ document.getElementById('demo86').innerHTML ="fruits4.set(Apple): "+ fruits4.get
 							const myCar1 = new car3 ("Ford", 2014);
 						  const myCar2 = new car3 ("Volvo", 2020);
 						document.getElementById("demo98").innerHTML = myCar1.name + " " + myCar2.name;
-					class Car4 {
-						constructor(name, year) {
-							this.name = name;
-							this.year = year;
-						}					
-			  age() {
-				const date = new Date();
-			    return date.getFullYear() - this.year;
-			  }
-			}
-			  const myCar3 = new Car4 ("BMW", 2010);
-		  document.getElementById("demo99").innerHTML = "My car is " + myCar3.age() + " years old";
-		let text38 = "{'employess':[" +
-	"{'fname': 'Ibrahim', 'lname':'khan'}," + 
-	"{'fname': 'Shihab', 'lname':'khan'}," +
-	"{'fname': 'Salman', 'lname': 'khan'}]}";
-const obj = JSON.parse(text38);
-document.getElementById('demo100').innerHTML = obj.employess[1].fname + " " + obj.employess[1].lname;
+					
+
+  let sentence = "learn with Sumit is all about teaching web development skills and techniques in an efficient and practical manner. if \
+     you are just getting started in web development, Learn with Sumit has all the tools you need to learn the newest and most popular \
+	  technologies to convert you from a no-stack to a full-stack developer. Lear with Sumit also deep dives onto advanced topics using \
+	    the latest best practices for you seasoned web developer.";
+
+		  let matchs = sentence.match(/sumit/ig);
+
+		  document.getElementById('demo100').innerHTML = matchs;
+
+		
 											
 
 
